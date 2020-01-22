0e8ea8c6c8d33417c4f5b31d9ef3e683b1390dee
HOLFF <leerharter@gmail.com>
2020-01-22 08:03:37 +0100

    final changes

--- src/Schöpf/Sort/Quick_Sort.java
+++ src/Schöpf/Sort/Quick_Sort.java
@@ -4,12 +4,12 @@ import Schöpf.HF.HelpFunctions;
 
 public class Quick_Sort {
     public static void main(String[] args){
-        Integer[] arr = HelpFunctions.befüllen(13);
+        Integer[] arr = HelpFunctions.befüllen(10000000);
         System.out.println("Before Quick sort:");
-        HelpFunctions.ausgabe(arr);
+        //HelpFunctions.ausgabe(arr);
         long time = HelpFunctions.microtime(arr,Quick_Sort::qs);
         System.out.println("After Quick sort:");
-        HelpFunctions.ausgabe(arr);
+        //HelpFunctions.ausgabe(arr);
         System.out.println("Vergangene Zeit in Microsekunden:"+ (time));
     }
     public static void qs(Integer[] arr){
